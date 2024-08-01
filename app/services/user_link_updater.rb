3823281384630b3ac8a18d95f47dd38425f833f3
safych <v7lad@ukr.net>
2024-08-01 22:00:25 +0300

    Added method url_format_verification

--- app/services/user_link_updater.rb
+++ app/services/user_link_updater.rb
@@ -1,3 +1,5 @@
+require "./app/services/validators/url_validator.rb"
+
 class UserLinkUpdater
   attr_reader :user_link, :user_id, :new_url, :error
 
@@ -14,11 +16,22 @@ class UserLinkUpdater
   private
 
   def update
-    if verification_user
-      unless @user_link.update(url: new_url)
+    if url_format_verification && verification_user
+      unless @user_link.update(url: @new_url)
         @error = I18n.t("services.user_link_updater.user_link_was_not_successful_updated")
       end
     end
+  end
+
+  def url_format_verification
+    url_validator = UrlValidator.new(@new_url)
+    unless url_validator.valid?
+      url_validator.errors.full_messages.each do |message|
+        @error = message
+      end
+      return false
+    end
+    true
   end
 
   def verification_user
