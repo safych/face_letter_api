c2880b4e6096c2b0e216f97efd7b16b0a779711d
safych <v7lad@ukr.net>
2024-08-02 12:57:45 +0300

    Generated configuration files and added FactoryBot to RSpec.

---
+++ spec/factories.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  
+  factory :user do
+    name { "Joe" }
+    surname { "Clooney" }
+    email { Faker::Internet.email }
+    password_digest { BCrypt::Password.create("Test1234!") }
+  end
+
+  factory :user_token do
+    association :user
+    token { SecureRandom.hex(30) }
+  end
+
+  factory :user_link do
+    association :user
+    url { Faker::Internet.url }
+  end
+end
