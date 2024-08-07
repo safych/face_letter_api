f73dc169b7e0dbc510007bda35b41cf02c952eba
safych <v7lad@ukr.net>
2024-08-07 15:38:26 +0300

    Updated controller functional. Created service for update user data and locales for it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
-  before_action :set_user
+  before_action :authenticate_user!
 
   def update
-
+    user_updater = UserUpdater.new(user_params, get_token)
+    user_updater.call
+    if user_updater.message[:done]
+      render json: { message: user_updater.message[:done] }, status: :ok
+    else
+      render json: { error: user_updater.message[:error] }, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
+  def user_params
+    params.require(:user).permit(:new_password, :current_password, :name, :surname)
   end
 end
---
+++ app/services/user_updater.rb
@@ -0,0 +1,76 @@
+require "./app/services/validators/password_validatior.rb"
+
+class UserUpdater
+  attr_reader :params, :token, :message, :user
+
+  def initialize(params, token)
+    @params = params
+    @token = token
+    @message = Hash.new
+  end
+
+  def call
+    get_user
+  end
+
+  private
+
+  def get_user
+    user_token = UserToken.find_by(token: @token)
+    if user_token
+      @user = User.find(user_token.user_id)
+      if @user
+        update
+      else
+        @message[:error] = I18n.t("services.user_updater.user_not_found")
+      end
+    else
+      @message[:error] = I18n.t("services.user_updater.not_correct_token")
+    end
+  end
+
+  def update
+    if @params[:name] || @params[:surname]
+      update_name_surname
+    elsif @params[:new_password]
+      update_password
+    end
+  end
+
+  def update_name_surname
+    if @user.update(name: @params[:name], surname: @params[:surname])
+      @message[:done] = I18n.t("services.user_updater.user_data_successful_updated")
+    else
+      @message[:error] = I18n.t("services.user_updater.user_data_did_not_update")
+    end
+  end
+
+  def update_password
+    if password_format_verification && current_password_verification
+      if @user.update(password_digest: @params[:new_password])
+        @message[:done] = I18n.t("services.user_updater.user_password_successful_updated")
+      else
+        @message[:error] = I18n.t("services.user_updater.user_password_did_not_update")
+      end
+    end
+  end
+
+  def password_format_verification
+    error_text = ""
+    password_validator = PasswordValidator.new(@params[:new_password])
+    unless password_validator.valid?
+      password_validator.errors.full_messages.each do |message|
+        error_text += "#{message}. "
+      end
+      @message[:error] = error_text
+      return false
+    end
+    true
+  end
+
+  def current_password_verification
+    return true if @user.authenticate(@params[:current_password])
+    @message[:error] = I18n.t("services.user_updater.not_correct_password")
+    false
+  end
+end
