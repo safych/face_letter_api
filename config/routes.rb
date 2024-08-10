8d31b2304d4030d8725b000069168bff5a328384
safych <v7lad@ukr.net>
2024-08-10 15:13:58 +0300

    Added routes for update email and reset password

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   delete "users/sign_out", to: "users/sessions#destroy", as: :destroy_user_session
 
   post "users", to: "users/registrations#create", as: :user_registration
+
+  patch "/users/update_email_token", to: "users/update_email_tokens#update", as: :user_update_email_token 
+  patch "/users/reset_password_token", to: "users/reset_password_tokens#update", as: :user_reset_password_token
+  patch "/users/email", to: "users/emails#update", as: :user_email
+  patch "/users/password", to: "users/passwords#update", as: :user_password
 end
