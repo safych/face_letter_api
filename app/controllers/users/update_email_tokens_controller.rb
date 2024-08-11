8ec2a4f2b1b32338e0f6aff8b950bad791b60ff9
safych <v7lad@ukr.net>
2024-08-11 12:29:35 +0300

    Changes due to file renaming

--- app/controllers/users/update_email_tokens_controller.rb
+++ app/controllers/users/update_email_tokens_controller.rb
@@ -3,12 +3,12 @@ module Users
     before_action :authenticate_user!
 
     def update
-      update_email_token_creator = UpdateEmailTokenCreator.new(get_param, get_token)
-      update_email_token_creator.call
-      if update_email_token_creator.message[:done]
-        render json: { message: update_email_token_creator.message[:done] }, status: :ok
-      elsif update_email_token_creator.message[:error]
-        render json: { errors: update_email_token_creator.message[:error] }, status: :unprocessable_entity
+      update_email_token_updater = UpdateEmailTokenUpdater.new(get_param, get_token)
+      update_email_token_updater.call
+      if update_email_token_updater.message[:done]
+        render json: { message: update_email_token_updater.message[:done] }, status: :ok
+      elsif update_email_token_updater.message[:error]
+        render json: { errors: update_email_token_updater.message[:error] }, status: :unprocessable_entity
       end
     end
 
---
+++ app/services/update_email_token_updater.rb
@@ -0,0 +1,46 @@
+class UpdateEmailTokenUpdater
+  attr_reader :param, :message, :user, :token
+
+  def initialize(param, token)
+    @param = param
+    @message = Hash.new
+    @token = token
+  end
+
+  def call
+    search_user_by_token
+  end
+
+  private
+
+  def search_user_by_token
+    user_token = UserToken.find_by(token: @token)
+    if user_token
+      @user = User.find(user_token.user_id)
+      if @user
+        send_token_for_update_email
+      else
+        @message[:error] = I18n.t("services.update_email_token_creator.user_not_found")
+      end
+    else
+      @message[:error] = I18n.t("services.update_email_token_creator.not_correct_token")
+    end
+  end
+
+  def send_token_for_update_email
+    if current_password_verification
+      @user.update_email_token = SecureRandom.hex(3)
+      @user.update_email_sent_at = DateTime.now
+      if @user.save
+        @message[:done] = I18n.t("services.update_email_token_creator.token_was_updated")
+        UserMailer.update_email(@user.email, @user.update_email_token).deliver_now
+      end
+    end
+  end
+
+  def current_password_verification
+    return true if @user.authenticate(@param[:current_password])
+    @message[:error] = I18n.t("services.update_email_token_creator.not_correct_password")
+    false
+  end
+end
